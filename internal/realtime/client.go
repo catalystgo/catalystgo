fde08011605d36f63daba09853f9f8ad6727e392
escalopa <ahmad.helaly.dev@gmail.com>
2024-05-18 13:12:59 +0300

    feat: add ETCD client

---
+++ internal/realtime/client.go
@@ -0,0 +1,10 @@
+package realtime
+
+import "context"
+
+type Client interface {
+	GetValue(ctx context.Context, key string) (Value, error)
+	SetValue(ctx context.Context, key string, value string) error
+	Watch(ctx context.Context, key string, cb func(old Value, new Value))
+	Close() error
+}
